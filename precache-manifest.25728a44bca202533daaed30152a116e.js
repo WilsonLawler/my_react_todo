786835af041121af53eaf1ff6ccbd31a681fb0ad
William Huang <liungwave910743@gmail.com>
2018-12-28 22:39:25 +0800

    Finished building app.

---
+++ precache-manifest.25728a44bca202533daaed30152a116e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "be3a46b18458e54a6ddd",
+    "url": "./static/js/main.be3a46b1.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "./static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "be3a46b18458e54a6ddd",
+    "url": "./static/css/main.2d7b2360.chunk.css"
+  },
+  {
+    "revision": "44768a337ded689c073580e74b588f93",
+    "url": "./index.html"
+  }
+];
